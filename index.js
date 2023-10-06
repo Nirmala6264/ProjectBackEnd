c728b34e329a84ad7acb62d1e572fbdc64a79096
Nirmala6264 <nirmalamanikandan21@gmail.com>
2023-10-06 23:49:28 +0530

    Updated

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ import hallrouter from './Routers/hallBooking.router.js'
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+               
 const PORT = 4000;
+app.use('/', (req,res) => {
+    res.send("WELCOME TO HALLBOOKING APP")
+})
 app.use('/api',hallrouter)
 app.listen(PORT, () => {
     console.log("App is Listening-",PORT);
 })
+
+
+
+
+
 
 
